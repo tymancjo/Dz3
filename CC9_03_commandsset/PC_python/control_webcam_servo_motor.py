3db1038ea06d72c2f7de92d90fd5d0a499fc2faa
Tomasz Tomanek <tymancjo@pop-os.localdomain>
2020-10-24 11:41:39 +0200

    modof py track to use object size

--- CC9_03_commandsset/PC_python/control_webcam_servo_motor.py
+++ CC9_03_commandsset/PC_python/control_webcam_servo_motor.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import serial
+import math
 
 # cams_test = 10
 # for i in range(0, cams_test):
@@ -17,8 +18,12 @@ except:
 
 cap = cv2.VideoCapture(2)
 
-cap.set(3, 480)
-cap.set(4, 320)
+W, H = (640, 480)
+
+cap.set(3, W)
+cap.set(4, H)
+
+diag = math.sqrt(W**2 + H**2)
 
 _, frame = cap.read()
 rows, cols, _ = frame.shape
@@ -28,7 +33,8 @@ center = int(cols / 2)
 position = 90 # degrees
 loopdelay = 0
 loop = 0
-
+size = 0
+size_treshold = 5
 
 while True:
     _, frame = cap.read()
@@ -43,27 +49,45 @@ while True:
     
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
     
-    for cnt in contours:
+    # for cnt in contours:
+    #     (x, y, w, h) = cv2.boundingRect(cnt)
+
+    #     # calculating the size
+    #     size = 100 * math.sqrt(w**2 + h**2) / diag
+    #     x_medium = int((x + x + w) / 2)
+    #     break
+    
+    if len(contours) > 0:
+        cnt = contours[0]
         (x, y, w, h) = cv2.boundingRect(cnt)
+
+        # calculating the size
+        size = 100 * math.sqrt(w**2 + h**2) / diag
+        x_medium = int(x + w / 2)
+        y_medium = int(y + h / 2)
+    
+    
+    if size >= size_treshold:
+        cv2.line(frame, (x_medium, 0), (x_medium, H), (0, 255, 0), 2)
+        cv2.line(frame, (0, y_medium), (W, y_medium), (0, 255, 0), 2)
         
-        x_medium = int((x + x + w) / 2)
-        break
-    
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
-    
+        # moving around
+        _error = center - x_medium;
+
+        if abs(_error) > 30:
+            message = f'<1,0,{_error/10},300>'
+
+            if loop > loopdelay:    
+                loop = 0
+                try:
+                    ser.write(message.encode('utf-8'))
+                    print(message.encode('utf-8'))
+                except:
+                    pass
+
+    cv2.putText(frame,str(int(size)), (10,10), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
     cv2.imshow("Frame", frame)
     
-    # moving around
-    _error = center - x_medium;
-
-    if abs(_error) > 30:
-        message = f'<1,0,{_error/10},300>'
-
-        if loop > loopdelay:    
-            loop = 0
-            ser.write(message.encode('utf-8'))
-            print(message.encode('utf-8'))
-
     loop += 1;
 
     key = cv2.waitKey(1)
